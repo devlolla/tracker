9f06253403805bcba49f16360f610aea89225d81
devlolla <devlolla@outlook.com>
2021-10-15 04:52:35 -0300

    add ghost button :ghost:

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,20 +1,45 @@
 import { ButtonHTMLAttributes, ReactNode } from "react"
+import tw from 'tailwind-styled-components'
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
+  variant?: 'ghost'
 }
 
-export const Button = ({ children, ...props }) => {
-  return (
-    <button
-      {...props}
-      className="
-        bg-purple-400 h-10 
-        text-white font-bold 
-        hover:bg-purple-500 
-        disabled:bg-gray-200
-        disabled: cursor-not-allowed">
+export const Button = ({ children, variant, ...props }: Props) => {
+  if (variant === 'ghost') return (
+    <GhostButton {...props}>
       {children}
-    </button>
+    </GhostButton>
+  )
+  return (
+    <MainButton {...props}>
+      {children}
+    </MainButton>
   )
 }
+
+export const BaseButton = tw.button`
+  h-10
+  px-8
+  text-white 
+  font-bold 
+  disabled:cursor-not-allowed
+  disabled:bg-gray-200
+`
+
+/* 
+Usamos esse formato para extender as propriedades de um componente styled
+igual quando a gente fazia com os componentes do materialUi
+*/
+export const MainButton = tw(BaseButton)`
+  bg-purple-400
+  hover:bg-purple-500
+
+`
+
+export const GhostButton = tw(BaseButton)`
+  bg-transparent
+  hover:bg-gray-50
+  text-purple-500
+`
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,17 @@
+import { Button } from "../../components/atoms/Button"
+import { TemplateContainer } from "../../components/templates"
+
 export const Dashboard = () => {
   return (
-    <div>
-      Hello !
-    </div>
+    <TemplateContainer>
+      <header className="w-full max-w-screen-xl flex items-center justify-between h-1/6">
+        <span className="font-bold text-2xl">Expanse Tracker</span>
+        <nav className="flex gap-4">
+          <Button> Adicionar nova conta</Button>
+          <Button variant='ghost'> Sair </Button>
+        </nav>
+
+      </header>
+    </TemplateContainer>
   )
 }
